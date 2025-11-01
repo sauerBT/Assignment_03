d6ebeee0323f191d1d8f217e0c5c6b259a2abe51
sauerBT <briansauerborn@gmail.com>
2025-11-01 11:27:16 -0400

    Updated constructor checks, added logic body to playGame

--- code/src/cs3500/pyramidsolitaire/controller/PyramidSolitaireTextualController.java
+++ code/src/cs3500/pyramidsolitaire/controller/PyramidSolitaireTextualController.java
@@ -10,20 +10,38 @@ public class PyramidSolitaireTextualController implements PyramidSolitaireContro
     Appendable outStream;
 
     public PyramidSolitaireTextualController(Readable inStream, Appendable outStream) {
+<<<<<<< Updated upstream
         if (inStream != null && outStream != null) {
+=======
+        if ((inStream != null) && (outStream != null)) {
+>>>>>>> Stashed changes
             this.inStream = inStream;
             this.outStream = outStream;
         } else {
             if (inStream == null) {
+<<<<<<< Updated upstream
                 throw new IllegalArgumentException("No input stream supplied to controller.");
             } else {
                 throw new IllegalArgumentException("No output stream supplied to controller.");
             }
         }
 
+=======
+                throw new IllegalArgumentException("Must provide an instream!");
+            } else {
+                throw new IllegalArgumentException("Must provide an outstream!");
+            }
+        }
+>>>>>>> Stashed changes
     }
 
     // TODO
     @Override
-    public <K> void playGame(PyramidSolitaireModel<K> model, List<K> deck, boolean shuffle, int numRows, int numDraw) {};
+    public <K> void playGame(PyramidSolitaireModel<K> model, List<K> deck, boolean shuffle, int numRows, int numDraw) {
+        if (model != null) {
+            model.startGame(deck, shuffle, numRows, numDraw);
+        } else {
+            throw new IllegalArgumentException("Provided model cannot be null!");
+        }
+    };
 }
